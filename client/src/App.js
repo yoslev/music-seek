899f81cf259bb78855eabca67a7e2a9aeda67254
yossil <Yossi.Lev@nice.com>
2022-06-30 19:57:30 +0300

    Open detailed User from table row

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 // client/src/App.js
 
 import "./App.css";
-import UserTables from "./components/userTable";
 
 import {BrowserRouter, Routes, Route, Outlet, useParams} from "react-router-dom";
 import Navbar from "./components/navbar"
@@ -9,24 +8,10 @@ import Home from "./pages/navbar/Home"
 import Users from "./pages/navbar/Users"
 import Settings from "./pages/navbar/Settings"
 import Admin from "./pages/navbar/Admin"
+import User from "./components/user";
 
 const App = () => {
 
-
-    let x = (
-        <div className="App">
-            <p></p>
-            <br/>
-            <br/>
-            <br/>
-            <hr/>
-            <p>User table</p>
-            <UserTables/>
-        </div>
-    );
-    /*
-
-    */
     return (
         <div>
             <BrowserRouter>
@@ -46,11 +31,6 @@ const App = () => {
         </div>
     );
 } // App()
-
-function User(props) {
-    let { id } = useParams();
-    return <h1>USER id: {id}</h1>;
-}
 
 function Layout() {
     return (
--- client/src/components/user.js
+++ client/src/components/user.js
@@ -1,23 +1,39 @@
-import React, { useState,useEffect }  from "react";
+import React, {useState, useEffect, useRef} from "react";
+import {useParams} from "react-router-dom";
+import {getAgeClassById, getGenreById, getInstrumentById, getPlayerLevelById, getRegionById} from "../entities/enums";
 
 export default function User() {
-    const [users, setUsers] = useState(null);
+    const [user, setUser] = useState(null);
+    const [uid, setUid] = useState(null);
+
+    let { id } = useParams();
 
     useEffect(() => {
-        fetch("/api/user")
-            .then((res) => {
-                return res.json()
-            })
-            .then((users) => {
-                setUsers(users)
-            });
-    }, []);
+            fetch(`/api/v01/users/${id}`)
+                .then((res) => {
+                    return res.json()
+                })
+                .then((u) => {
+                     setUser(u);
+                });
+    }, [id]);
+
+
+    if (!user) {
+        return <h1>{id}</h1>; //'Loading..'
+    }
 
     return (
-        <div className="App">
-            <header className="App-header">
-                {!users ? "XXXXX" : users.map(u => <div key={u.id}> {u.firstName} {u.lastName}</div>)}
-            </header>
+        <div className="user">
+            <h1>USER id: {id}</h1>
+            <p>first name    : {user.firstName}</p>
+            <p>last name     : {user.lastName}</p>
+            <p>eMail         : {user.email}</p>
+            <p>1st instrument: {getInstrumentById(user.mainPayingInstrumentId)} Level: {getPlayerLevelById(user.mainPayingInstrumentLevel)}</p>
+            <p>2nd instrument: {getInstrumentById(user.secondPayingInstrumentId)} Level: {getPlayerLevelById(user.secondPayingInstrumentLevel)}</p>
+            <p>Genre         : {getGenreById(user.musicGenres)}</p>
+            <p>Region        : {getRegionById(user.region)}</p>
+            <p>Age           : {getAgeClassById(user.ageClass)}</p>
         </div>
     );
 }
